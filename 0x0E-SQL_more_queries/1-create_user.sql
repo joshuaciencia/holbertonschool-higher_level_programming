be32d9edd4d9b603ade43e5fcdbe9168e0122c37
josh <joshuaciencia2016@gmail.com>
2019-11-28 23:56:33 +0000

    create user and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates an user and grants all privileges to him
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
