48c8d3409732792b3d4392861c43a1cf91068c7b
josh <joshuaciencia2016@gmail.com>
2019-11-29 00:53:40 +0000

    create db and table to it with primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- creates database and a table for it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
