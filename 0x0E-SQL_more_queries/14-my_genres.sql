464f11d230a712dd861f0a98b00bf4c1cfadbde7
josh <joshuaciencia2016@gmail.com>
2019-11-30 20:55:41 +0000

    task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- list all genres of the show Dexter in hbtn_0d_tvshows database
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = 
+(SELECT tv_shows.id FROM tv_shows
+WHERE tv_shows.title = 'Dexter');
