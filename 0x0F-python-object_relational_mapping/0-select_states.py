475004d3fda56e9a70b7a6c41317e4fbc60d75c8
josh <joshuaciencia2016@gmail.com>
2019-12-14 23:18:31 +0000

    task 0 pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""script to get data in a db """
 
 if __name__ == '__main__':
-    import MySQLdb, sys
-    db = MySQLdb.connect(host='localhost',
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    """ get data from db """
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id");
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     states = cur.fetchall()
     for state in states:
         print(state)
