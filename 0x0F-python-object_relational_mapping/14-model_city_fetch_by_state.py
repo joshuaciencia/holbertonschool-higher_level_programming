150c977107382ecfb49883ea8a9fafeca65c6117
josh <joshuaciencia2016@gmail.com>
2019-12-15 01:08:07 +0000

    final mandatory task 14 and model_city.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    for c, s in s.query(City, State).\
+            filter(State.id == City.state_id).\
+            all():
+                print("{}: ({}) {}".format(s.name, c.id, c.name))
+    s.close()
