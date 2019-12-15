31bab2c82f090bb95c34ed2e58f9ce0e6aefad16
josh <joshuaciencia2016@gmail.com>
2019-12-15 00:15:08 +0000

    task 13, delete instances with 'a'

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+deletes all State objects with a name containing the letter a
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                    sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    instances = s.query(State).filter(State.name.like('%a%'))
+    for inst in instances:
+        s.delete(inst)
+    s.commit()
+    s.close()
