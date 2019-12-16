1453a4a278def460b6fdc8d148ed03fed2210b8c
josh <joshuaciencia2016@gmail.com>
2019-12-16 02:18:35 +0000

    task 101 [Fix] -> properly tab

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
                                                     sys.argv[2],
                                                     sys.argv[3]),
         pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).join(City).\
