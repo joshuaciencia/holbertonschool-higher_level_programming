ab9dbb4623680556368b499c91a3f0cb5f6124cc
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 20:07:38 +0000

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+""" Displays url response body with error
+of http status code
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    req = requests.get(argv[1])
+    code = req.status_code
+    if code == 200:
+        print(req.content.decode('utf-8'))
+    else:
+        print(code)
