e0fdb45aab177ef3a386ebb4135f66e0d578cc15
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 18:50:07 +0000

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-import urllib.error.HTTPError as err
+""" Displays url response body with error
+of http status code
+"""
+import urllib
+from sys import argv
+import urllib.request as r
 
 
 if __name__ == '__main__':
+    req = r.Request(argv[1])
+    try:
+        with r.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
