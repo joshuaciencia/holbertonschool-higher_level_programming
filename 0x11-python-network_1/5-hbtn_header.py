1ad2671605ead7592245cd6dfbe5177ff772cf72
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 19:44:20 +0000

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""
+displays header value of url
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    res = requests.get(argv[1])
+    print(res.headers.get('X-Request-Id'))
