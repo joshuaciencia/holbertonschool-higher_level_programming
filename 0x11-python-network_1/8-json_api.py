a11105586c50e038840f099e43c15eab7c80f688
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 20:45:11 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""
+send POST req to url with
+letter param
+"""
+import requests
+from sys import argv
+
 if __name__ == '__main__':
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    data = {
+        'q': q
+    }
+    req = requests.post('http://fb67ecd083dd.19.hbtn-cod.io:\
+5000/search_user', data)
+    try:
+        json = req.json()
+        if not len(json):
+            print("Not result")
+        else:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+    except Exception as e:
+        print("Not a valid JSON")
