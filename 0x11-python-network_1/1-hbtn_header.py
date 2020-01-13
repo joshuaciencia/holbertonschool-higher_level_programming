b291668f4bd80380607c7882a5d8614ccc8d07de
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 17:24:09 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,9 @@
 """
 returns the specified header request
 """
-import os
 import urllib.request as u
-
+import sys
 
 if __name__ == '__main__':
-    with u.urlopen(os.sys.argv[1]) as res:
+    with u.urlopen(sys.argv[1]) as res:
         print(res.info().get('X-Request-Id'))
