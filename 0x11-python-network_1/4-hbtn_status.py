871d329782ac4f101ea67a287014fc5a01f529b0
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 19:06:36 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+""" fetches from https://intranet.hbtn.io/status
+"""
+import requests
+
+
 if __name__ == '__main__':
+    res = requests.get('https://intranet.hbtn.io/status').content
+    print("Body response:")
+    print("\t- type: {}".format(type(str(res))))
+    print("\t- content: {}".format(res.decode('utf-8')))
