f02dfa0f6738b44b5bbd23393341b667aeab7672
josh <joshuaciencia2016@gmail.com>
2020-01-13 11:56:47 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+import urllib.request as u
+
+
 if __name__ == '__main__':
+    with u.urlopen('https://intranet.hbtn.io/status') as res:
+        html = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(str(html)))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
