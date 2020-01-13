1ccb61ca477707024aeb353b0223f009ff7788c2
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 19:49:22 +0000

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""
+displays the email return by a post request
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    data = {
+        'email': argv[2]
+    }
+    req = requests.post(argv[1], data)
+    print(req.content.decode('utf-8'))
