ecd15add246e1dd8cb659a171cf5d98829b09709
joshuaciencia <joshuaciencia2016@gmail.com>
2020-01-13 22:24:12 +0000

    task 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""
+sends a search request to the Star Wars API
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    req = requests.get('https://swapi.co/api/people/?search={}'
+                       .format(argv[1]))
+    l = req.json().get('results')
+    names = []
+    for item in l:
+        if 'name' in item:
+            names.append(item.get('name'))
+    print('Number of results: {}'.format(len(names)))
+    for name in names:
+        print(name)
