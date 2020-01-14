ee4c036beee63dd6fe94035eebb0b853af78cfa1
josh <joshuaciencia2016@gmail.com>
2020-01-14 04:48:14 +0000

    task 102 now using count attr

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -8,22 +8,17 @@ from sys import argv
 
 if __name__ == '__main__':
     url = 'https://swapi.co/api/people/?search={}'.format(argv[1])
-    names = {}
+    req = requests.get(url)
+    print('Number of results: {}'.format(req.json().get('count')))
     while url:
         req = requests.get(url)
         l = req.json().get('results')
         for item in l:
             if 'name' in item:
                 film_urls = item.get('films')
-                films = []
+                print(item.get('name'))
                 for f in film_urls:
                     r = requests.get(f)
                     title = r.json().get('title')
-                    films.append(title)
-                names[item.get('name')] = films
+                    print("\t{}".format(title))
         url = req.json().get('next')
-    print('Number of results: {}'.format(len(names)))
-    for name, films in names.items():
-        print(name)
-        for f in films:
-            print("\t{}".format(f))
