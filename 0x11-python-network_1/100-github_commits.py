eb48d6ac090bee5b3c0cebf6049d874292879a9e
josh <joshuaciencia2016@gmail.com>
2020-01-14 03:30:44 +0000

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(argv[2], argv[1]))
+    l = req.json()
+    for i in range(10):
+        print("{}: {}".format(l[i].get('sha'),
+              l[i].get('author').get('login')))
