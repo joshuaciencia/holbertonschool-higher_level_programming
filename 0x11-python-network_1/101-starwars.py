2c6bc393d849d5abd6982464854c7b83b1c3e9c0
josh <joshuaciencia2016@gmail.com>
2020-01-14 04:09:16 +0000

    task 101

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+"""
+get data from SW API with pagination
+"""
+import requests
+from sys import argv
+
+
 if __name__ == '__main__':
+    url = 'https://swapi.co/api/people/?search={}'.format(argv[1])
+    names = []
+    while url:
+        req = requests.get(url)
+        l = req.json().get('results')
+        for item in l:
+            if 'name' in item:
+                names.append(item.get('name'))
+        url = req.json().get('next')
+    print('Number of results: {}'.format(len(names)))
+    for name in names:
+        print(name)
