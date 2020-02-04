d50504fd686aec94688e2fdec4129d7233a3fa1a
joshua <joshuaciencia2016@gmail.com>
2020-02-04 16:45:19 -0500

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arg = process.argv;
+const x = parseInt(arg[2]);
+if (!x) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
