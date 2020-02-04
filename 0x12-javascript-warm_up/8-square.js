3fe05c049f41b1c0a30e5359cd5bfef4b26e9f9e
joshua <joshuaciencia2016@gmail.com>
2020-02-04 16:55:06 -0500

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const arg = process.argv;
+const x = parseInt(arg[2]);
+if (!x) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let line = '';
+    for (let j = 0; j < x; j++) {
+      line = line + 'X';
+    }
+    console.log(line);
+  }
+}
