4522deae153622e88bd31e5e13686d5596e2c5db
joshua <joshuaciencia2016@gmail.com>
2020-02-04 17:21:20 -0500

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const arg = process.argv;
+const num = parseInt(arg[2]);
+if (!num) {
+  console.log(1);
+} else {
+  let result = num;
+  for (let i = num - 1; i > 1; i--) {
+    result *= i;
+  }
+  console.log(result);
+}
