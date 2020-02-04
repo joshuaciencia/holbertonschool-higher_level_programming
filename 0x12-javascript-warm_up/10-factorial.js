f8c6daf8f5be9d46bccf86efe8a322f86f07dd08
joshua <joshuaciencia2016@gmail.com>
2020-02-04 17:32:35 -0500

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 const arg = process.argv;
 const num = parseInt(arg[2]);
+
+function factorial (val, i) {
+  if (i <= 1) {
+    return (val);
+  }
+  return (factorial(val * i, i - 1));
+}
+
 if (!num) {
   console.log(1);
 } else {
-  let result = num;
-  for (let i = num - 1; i > 1; i--) {
-    result *= i;
-  }
-  console.log(result);
+  console.log(factorial(num, num - 1));
 }
