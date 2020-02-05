69742b819ddb2c1fbb935cd396cabf061d294df9
joshua <joshuaciencia2016@gmail.com>
2020-02-04 22:26:38 -0500

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const arg = process.argv;
+if (arg.length < 4) {
+  console.log('0');
+} else {
+  const nums = [];
+  for (let i = 2; i < arg.length; i++) {
+    nums.push(parseInt(arg[i]));
+  }
+  const largest = Math.max(...nums);
+  nums.splice(nums.indexOf(largest), 1);
+  console.log(Math.max(...nums));
+}
