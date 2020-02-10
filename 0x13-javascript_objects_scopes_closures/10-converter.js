ec44386fe16bfc81c4228d2b49f81c2db5f0f198
joshuaciencia <joshuaciencia@gmail.com>
2020-02-10 15:41:27 -0500

    task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,20 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function (num) {
-    
-  }
-}
+    let result = '';
+    while (num > base) {
+      const remainder = num % base;
+      if (remainder >= 10) {
+        result += String.fromCharCode(97 + (remainder - 10));
+      } else {
+        result = remainder + result;
+      }
+      num = num / base;
+    }
+    if (num >= 10) {
+      return String.fromCharCode(97 + (num - 10)) + result;
+    }
+    result = Math.floor(num) + result;
+    return result;
+  };
+};
