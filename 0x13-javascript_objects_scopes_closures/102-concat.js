4dc983886c55ed67f818ad404f974a720318a103
joshuaciencia <joshuaciencia@gmail.com>
2020-02-10 17:08:53 -0500

    task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const argv = process.argv;
+const fileA = argv[2];
+const fileB = argv[3];
+const fileC = argv[4];
+let content = '';
+content += fs.readFileSync(fileA);
+content += fs.readFileSync(fileB);
+fs.writeFile(fileC, content, function (err) { if (err) throw err; });
