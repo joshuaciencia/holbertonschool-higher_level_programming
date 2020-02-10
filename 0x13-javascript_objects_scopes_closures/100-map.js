76ff5438c8bc21528476cf5f45ecad0c6ff29614
joshuaciencia <joshuaciencia@gmail.com>
2020-02-10 16:10:21 -0500

    task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
+const new_list = list.map(x => x * list.indexOf(x));
 console.log(list);
-console.log(list.map(x => x * list.indexOf(x)));
+console.log(new_list);
