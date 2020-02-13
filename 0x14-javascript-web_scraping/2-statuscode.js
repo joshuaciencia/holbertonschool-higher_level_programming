8a8120869f0bf21080aa471eca2060bc8b031bc0
joshua <joshuaciencia2016@gmail.com>
2020-02-13 02:49:30 +0000

    task_2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const argv = process.argv;
+const request = require('request');
+request(argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('code: ' + response.statusCode);
+});
